7405f0fcd527c7ece0ca259a8e11b14ee58e7eb3
Constantin Capatina <constantin.sorin.capatina@gmail.com>
2018-03-03 14:29:46 +0200

    Configure app boot

--- Gemfile
+++ Gemfile
@@ -5,15 +5,16 @@ ruby '2.5.0'
 source 'https://rubygems.org'
 
 gem 'sinatra'
-
+gem 'json'
 gem 'jsonapi-rb'
+gem 'rack_csrf'
 
 # When developing an app locally you can use SQLite which is a relational
 # database stored in a file. It's easy to set up and just fine for most
 # development situations.
 group :development do
   gem 'pry'
-
+  gem 'sinatra-reloader'
   # Enforce standard code formatting
   gem 'rubocop', require: false
 end
--- app.rb
+++ app.rb
@@ -3,6 +3,27 @@
 require 'bundler'
 Bundler.require
 
-get '/' do
-  { jsonapi: { version: "1.0"} }
+require 'rack/csrf'
+
+module GripitWeb
+  class App < Sinatra::Base
+    set :root, File.dirname(__FILE__)
+
+    enable :logging
+
+    use Rack::Session::Cookie, :secret => "TODO: CHANGE ME"
+    use Rack::Csrf, :raise => true
+
+    configure :development do
+      require "sinatra/reloader"
+      register Sinatra::Reloader
+    end
+
+    get "/" do
+      { jsonapi: { version: "1.0" } }.to_json
+    end
+
+    get "/" do
+    end
+  end
 end
